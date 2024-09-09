ee98f97aa37fbef320979ee8997a27347f1c5fde
Doc Wei <19955102698@163.com>
2024-09-09 10:19:04 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,57 @@
+
+var config = {
+    
+    texts: [
+        "Give me",     
+        "Dear YU",  
+        "Today is your Birthday",
+        "Since we met each other",
+        "It's the second birthday！",
+        "And there will always be in the future!",
+        "This year I have saw your",
+        "Dignified",
+        "Beautiful",
+        "And....emmm",
+        "Something strange",
+        "But all the looks I like",
+        "Have you got all the gifts?",
+        "Hope you eat well next year",
+        "You have strength to work",
+        "And to miss me",
+        "Happy Birthday!Dear!",
+        "Forever Beautiful!",
+    ],
+    imgs: {
+        "Give me": "./imgs/songxinxin.jpeg",//
+        "Dear YU": "./imgs/keai.jpg",//
+        "Today is your Birthday": "./imgs/shengri.jpeg",//
+        "Since we met each other": "./imgs/zaiyiqi.png",//
+        "It's the second birthday！": "./imgs/5.jpg",
+        "And there will always be in the future!":"./imgs/future.jpg",//
+        "This year I have saw your": "./imgs/yangzi.jpeg",//
+        "Dignified": "./imgs/dignified.jpg",//
+        "Beautiful": "./imgs/beautiful.jpg",//
+        "And....emmm":"./imgs/11.jpg",//
+        "Something strange": "./imgs/strange.jpg",//
+        "But all the looks I like": "./imgs/love.jpg",//
+        "Have you got all the gifts?": "./imgs/chuo.gif",//
+        "Hope you eat well next year": "./imgs/chihaodian.jpeg",//
+        "You have strength to work": "./imgs/work.jpg",//
+        "And to miss me": "./imgs/12.jpg",//
+        "Happy Birthday!Dear!": "./imgs/happy.jpg",//
+        "Forever Beautiful!":"./imgs/futureb.jpg",//
+    },
+    desc: {
+        turn_on: "Click Me",
+        play: "Play Music",
+        bannar_coming: "Put Colors",
+        balloons_flying: "Something else?",
+        cake_fadein: "Cake？",
+        light_candle: "Turn on Candles？",
+        wish_message: "Happy Birthday!!!",
+        story: "There are some memories"
+    },
+
+    // 结束语
+    loveText: 'By Wei 2024.9.13',
+};
