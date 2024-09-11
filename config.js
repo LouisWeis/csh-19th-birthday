f426f17d558613fc1ffc25b7073f7217371bff96
Doc Wei <19955102698@163.com>
2024-09-11 17:17:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ var config = {
     texts: [
         "Hello!",     
         "Dear YU",
-        "At a young age,I met you",
+        "At a young age I met you",
         "As the monsoon gently brushes by",
         "With time flies like a shuttle",
         "Time has turned back to the day that belongs to you",
         "Happy Birthday!",
         "Since we met each other",
-        "It's the second birthday！",
+        "It's the second birthday!",
         "And there will always be in the future!",
         "This year I have saw your",
         "Dignified",
@@ -29,41 +29,41 @@ var config = {
         "Forever Happy",
     ],
     imgs: {
-        "Hello!": "./imgs/songxinxin.jpeg",//
-        "Dear YU": "./imgs/keai.jpg",//
-        "At a young age,I met you":"./imgs/young.jpg",
-        "As the monsoon gently brushes by":"./imgs/fasttime.jpg",
-        "With time flies like a shuttle":"./imgs/fasttime2.jpg",
-        "Time has turned back to the day that belongs to you":"./imgs/belongs.jpg",
-        "Happy Birthday!": "./imgs/shengri.jpeg",//
-        "Since we met each other": "./imgs/zaiyiqi.png",//
-        "It's the second birthday！": "./imgs/5.jpg",
-        "And there will always be in the future!":"./imgs/future.jpg",//
-        "This year I have saw your": "./imgs/yangzi.jpeg",//
-        "Dignified": "./imgs/dignified.jpg",//
-        "Beautiful": "./imgs/beautiful.jpg",//
-        "And....emmm":"./imgs/11.jpg",//
-        "Something strange": "./imgs/strange.jpg",//
-        "But all the looks I like": "./imgs/love.jpg",//
+        "Hello!": "./imgs/songxinxin.jpeg",
+        "Dear YU": "./imgs/keai.jpg",
+        "At a young age I met you": "./imgs/young.jpg",
+        "As the monsoon gently brushes by": "./imgs/fasttime.jpg",
+        "With time flies like a shuttle": "./imgs/fasttime2.jpg",
+        "Time has turned back to the day that belongs to you": "./imgs/belongs.jpg",
+        "Happy Birthday!": "./imgs/shengri.jpeg",
+        "Since we met each other": "./imgs/zaiyiqi.png",
+        "It's the second birthday!": "./imgs/5.jpg",
+        "And there will always be in the future!": "./imgs/future.jpg",
+        "This year I have saw your": "./imgs/yangzi.jpeg",
+        "Dignified": "./imgs/dignified.jpg",
+        "Beautiful": "./imgs/beautiful.jpg",
+        "And....emmm":"./imgs/11.jpg",
+        "Something strange": "./imgs/strange.jpg",
+        "But all the looks I like": "./imgs/love.jpg",
          "Hope the happiness and joy always belong to you":"./imgs/xinfu.jpg",
-        "Have you got all the gifts?": "./imgs/chuo.gif",//
+        "Have you got all the gifts?": "./imgs/chuo.gif",
         "Hope you eat well next year": "./imgs/chihaodian.jpeg",//
         "You have strength to work": "./imgs/work.jpg",//
         "And...": "./imgs/12.jpg",//
         "To miss me": "./imgs/12.jpg",//
         "Happy Birthday!Little girl!": "./imgs/happy.jpg",//
         "Forever Beautiful!":"./imgs/futureb.jpg",//
-        "Forever Happy!":"./imgs/futurec.jpg:,
+        "Forever Happy!":"./imgs/futurec.jpg",
     },
     desc: {
         turn_on: "Click Me",
         play: "Play Music",
         bannar_coming: "Put Decorations",
         balloons_flying: "Something else?",
-        cake_fadein: "Cake？",
-        light_candle: "Turn on Candles？",
+        cake_fadein: "Cake?",
+        light_candle: "Turn on Candles?",
         wish_message: "Happy Birthday!!!",
-        story: "There are some memories"
+        story: "There are some memories",
     },
     loveText: 'By Wei 2024.9.9',
 };
